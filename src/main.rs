add63f9db79ebbe7bb0e2c86b18eecb9ca8f37ba
oliver.hines <oliver.hines@btinternet.com>
2022-01-02 17:34:18 +0000

    Added startup log

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use graphql::new_schema;
 use log::info;
-use log::LevelFilter::Trace;
+use log::LevelFilter::{Info};
 use server::start_server;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    env_logger::builder().filter(None, Trace).init();
+    env_logger::builder().filter(None, Info).init();
+    info!("Starting up...");
 
     let schema = new_schema();
     start_server(schema, "127.0.0.1:8000")
